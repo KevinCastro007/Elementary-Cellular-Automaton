47ad16bba7e0b4e10af8750268922b75a5788bd1
Kevin Castro <castrokevin7@gmail.com>
2015-09-12 15:09:43 -0600

    Changes

--- app.adb
+++ app.adb
@@ -16,7 +16,7 @@ procedure App is
     use Bit_IO;     
 
     T             : INTEGER;        -- Time T which defines the amount of states and the bitmap dimensions.
-    -- N             : INTEGER;        -- Number N to define the configuration.
+    N             : INTEGER;        -- Number N to define the configuration.
     N_Byte        : MY_BYTE;        -- Byte.
     N_Triad       : MY_TRIAD;       -- Triad.
 
@@ -31,7 +31,7 @@ procedure App is
 
     Image_Height : INTEGER;
     Image_Width  : INTEGER;
-    --File_Name    : STRING(1..8);
+    File_Name    : STRING(1..8);
     Color : PIXEL;
 
     -- Create the specific pattern's configuration.
@@ -101,7 +101,7 @@ procedure App is
         for I in 1..(N - 1)  loop
             if I = (N - 1) / 2 then
                 Temp_Automata.Bit := 1;
-                Set_Pixel(Image_Width, 0, (I - 1), WHITE);      
+                Set_Pixel(Image_Width, 0, I + 1, WHITE);      
             else
                 Temp_Automata.Bit := 0;
             end if;      
@@ -144,66 +144,43 @@ procedure App is
         end loop;        
     end Update_Sequence;
 
-
-    Temp_Name : Unbounded_String;   
-    Post_Name : STRING(1..11);    
-
 begin
--- <<Get_T>>
---     Put("Type T: "); Get(T);                        -- Data input: Time T.
---     if T < 0 then 
---         Put_Line("T should be greater than 0.");
---         goto Get_T;
---     end if;
--- <<Get_N>>
---     Put("Type N: "); Get(N);                        -- Data input: Number N.
---     if N < 0 or N > 255 then
---         Put_Line("N should be between 0 and 255.");
---         goto Get_N;
---     end if;
--- <<Get_Colors>>
---     Put_Line("Available colors -> Black:0, White:1, Red:2, Green:3, Blue:4, Yellow:5.");
---     Put("#Color: "); Get(Color);
---     if Color < 0 or Color > 5 then
---         Put_Line("Entry error. 0 <= #Color <= 5");
---         goto Get_Colors;
---     end if;  
+<<Get_T>>
+    Put("Type T: "); Get(T);                        -- Data input: Time T.
+    if T < 0 then 
+        Put_Line("T should be greater than 0.");
+        goto Get_T;
+    end if;
+<<Get_N>>
+    Put("Type N: "); Get(N);                        -- Data input: Number N.
+    if N < 0 or N > 255 then
+        Put_Line("N should be between 0 and 255.");
+        goto Get_N;
+    end if;
 
---     Put("Output file name: ");
---     Get(File_Name);       
-
-    T := 128;        
+    Put("Output file name: ");
+    Get(File_Name);  
     Image_Height := T;
     Image_Width := 2 * T;
     Generate_Image(Image_Height * Image_Width);     
     Initialize_Sequence(Image_Width);    
     Create_Configuration;
-    for N in 128..255 loop
-        Temp_Name := To_Unbounded_String("N");
-        Append(Temp_Name, To_Unbounded_String(Integer'Image(N / 100)));
-        Append(Temp_Name, To_Unbounded_String(Integer'Image((N mod 100) / 10)));
-        Append(Temp_Name, To_Unbounded_String(Integer'Image((N mod 100) mod 10)));
-        Append(Temp_Name, To_Unbounded_String(".ppm"));
-        Post_Name := To_String(Temp_Name);
-        Put(Post_Name);
-        Put(" ... ");
-        N_To_Byte(N, N_Byte);
-        Set_Configuration;  
-        Color := Generate_Pixel;
-    Main_Loop:
-        for I in 1..(Image_Height - 1) loop
-            if I + 1 = Image_Height then
-                Update_Sequence(I, Image_Width, Color); 
-                Put_PPM(Post_Name, Image_Height, Image_Width);
-                Put_Line("PPM generated successfully.");
-                Reset_Image;
-                Reset_Sequence(Image_Width);
-                exit Main_Loop;
-            else
-                Update_Sequence(I, Image_Width, Color); 
-            end if;
-        end loop Main_Loop;
-    end loop;
+    N_To_Byte(N, N_Byte);
+    Set_Configuration;  
+    Color := Generate_Pixel;
+Main_Loop:
+    for I in 1..(Image_Height - 1) loop
+        if I + 1 = Image_Height then
+            Update_Sequence(I, Image_Width, Color); 
+            Put_PPM(File_Name, Image_Height, Image_Width);
+            Put_Line("PPM generated successfully.");
+            Reset_Image;
+            Reset_Sequence(Image_Width);
+            exit Main_Loop;
+        else
+            Update_Sequence(I, Image_Width, Color); 
+        end if;
+    end loop Main_Loop;
 
 exception
     when Data_Error => Put_Line("Data Entry - Error");
